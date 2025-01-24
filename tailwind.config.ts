f26a6d125ce06cc2c2af0541b0c6b078b2461695
dana-yaniv <dana.yaniv@ornelix.com>
2025-01-24 17:03:32 +0200

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,31 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                DMSans: "'DM Sans', sans-serif",
+                GoogleSans: "'Google Sans', sans-serif",
+                Poppins: "'Poppins', sans-serif"
+            },
+            colors: {
+                White: '#ffffff',
+                LightWhite: '#E9EAEA',
+                WhiteSmoke: '#F6F7F7',
+                GreenBlue: "#2DB8A1",
+                Blue: "#0A66C2",
+                VeryLightGray: "#F4F5F5",
+                LightGray: "#949796",
+                Gray: "#737877",
+                DarkGray: "#484E4C"
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
